effe6046656ef7221066e17da69cc4b5bea48a19
david <dhalls@comecero.com>
2018-11-15 14:54:56 -0700

    Update settings to use sample files, update gulp to make local copies of settings files

---
+++ settings/app-SAMPLE.js
@@ -0,0 +1,17 @@
+﻿// Make a copy of this file and name it app.js, then place custom values to customize the app's behavior.
+// At runtime, the user's app settings will be automatically injected, providing the user's selections to the app.
+window.__settings = window.__settings || {}; window.__settings.app =
+    {
+        "account_id": "AA0000",
+        "test": true,
+        "company_name": null,
+        "use_square_images": true,
+        "footer_text": null,
+        "error_notifications": "moderate",
+        "show_promo_code": true,
+        "promo_code_name": "Promo Code",
+        "ask_save_cards": false,
+        "enable_languages": true,
+        "main_shopping_url": null,
+        "not_found_url": "https://example.com/404",
+    };
